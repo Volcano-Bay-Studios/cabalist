f273cf7b7a5642425956a5652b3781dcdfcd89bd
ModogTheDev <65304020+Modog500000@users.noreply.github.com>
2025-05-12 19:56:39 -0500

    ✨ Block

---
+++ src/main/java/xyz/volcanobay/cabalist/blocks/OscilistoneBlock.java
@@ -0,0 +1,22 @@
+package xyz.volcanobay.cabalist.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class OscilistoneBlock extends Block {
+    public OscilistoneBlock(Properties p_49795_) {
+        super(p_49795_);
+    }
+
+    @Override
+    protected void onPlace(BlockState p_60566_, Level p_60567_, BlockPos p_60568_, BlockState p_60569_, boolean p_60570_) {
+        super.onPlace(p_60566_, p_60567_, p_60568_, p_60569_, p_60570_);
+    }
+
+    @Override
+    protected void onRemove(BlockState p_60515_, Level p_60516_, BlockPos p_60517_, BlockState p_60518_, boolean p_60519_) {
+        super.onRemove(p_60515_, p_60516_, p_60517_, p_60518_, p_60519_);
+    }
+}
